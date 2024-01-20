57707eb70729dc49574a7fac1c338941448004f0
user <user@DESKTOP-ISMAEL>
2024-01-20 15:34:26 -0600

    Rpimer Commit

--- 012024/src/test/java/prueba2024/pruebaTest.java
+++ 012024/src/test/java/prueba2024/pruebaTest.java
@@ -6,8 +6,10 @@ import org.testng.annotations.Test;
 
 public class pruebaTest {
   @Test
-  public void f() {
+  public void f() throws InterruptedException {
 	  WebDriver driver = new ChromeDriver();
-	  driver.get("https://www.facebook.com");
+	  driver.get("https://www.mercadolibre.com");
+	  Thread.sleep(3000);
+		driver.close();
   }
 }
