7f2cbcaf3aa64818c33d28973cf13d2c2b81d5e7
user <user@DESKTOP-ISMAEL>
2024-01-27 10:09:02 -0600

    Rpimer Commit

---
+++ 012024/src/main/java/ejemplo/Java/Ejemplo_Loop_While.java
@@ -0,0 +1,15 @@
+package ejemplo.Java;
+
+public class Ejemplo_Loop_While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		
+		while (a <= 5) {
+			System.out.println("El valor de a: " + a);
+			a++; 
+			
+		}
+	}
+}
