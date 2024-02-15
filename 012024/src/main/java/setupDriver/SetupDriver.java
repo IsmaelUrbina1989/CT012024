e95b279f2ba83032da8fff71d25d8d0196cb6df2
user <user@DESKTOP-ISMAEL>
2024-02-14 21:36:37 -0600

    commit

--- 012024/src/main/java/setupDriver/SetupDriver.java
+++ 012024/src/main/java/setupDriver/SetupDriver.java
@@ -1,9 +1,9 @@
 package setupDriver;
 
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.time.Duration;
 
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebDriver;
 public class SetupDriver {
 	
 	public static WebDriver setupDriver() {		
-		WebDriver driver = new InternetExplorerDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		return driver;
