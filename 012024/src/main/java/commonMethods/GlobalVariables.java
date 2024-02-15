1ddbf072eacced904484dd2b1b55f94a3b1e2ebd
user <user@DESKTOP-ISMAEL>
2024-02-14 20:29:56 -0600

    commit

--- 012024/src/main/java/commonMethods/GlobalVariables.java
+++ 012024/src/main/java/commonMethods/GlobalVariables.java
@@ -3,8 +3,7 @@ package commonMethods;
 public class GlobalVariables {
 	
 	public static final String HOME_PAGE = "https://www.saucedemo.com/";
-	public static final String STANDAR_USER = "standard_user";
-	public static final String PASSWORD = "secret_sauce";
-	
+	public static final String STANDARD_USER = "standard_user";
+	public static final String PASSWORD = "secret_sauce";	
 	
 }
