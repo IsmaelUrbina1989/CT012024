733c48a29b4cbe3f33b142538478bbd6e9b1a091
user <user@DESKTOP-ISMAEL>
2024-02-20 18:30:16 -0600

    commit

--- 012024/src/main/java/commonMethods/GlobalVariables.java
+++ 012024/src/main/java/commonMethods/GlobalVariables.java
@@ -5,11 +5,6 @@ public class GlobalVariables {
 	public static final String HOME_PAGE = "https://www.saucedemo.com/";
 	public static final String STANDARD_USER = "standard_user";
 	public static final String PASSWORD = "secret_sauce";	
-	public static final String LOCKED_USER = "locked_out_user";
-	public static final String FIRSTNAME = "ISMAEL";
-	public static final String LASTNAME = "URBINA";
-	public static final String ZIPCODE = "07300";
-	
-	
+	public static final String LOCKED_USER = "locked_out_user";	
 	
 }
