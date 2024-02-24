bce8f6e8f72386db95b5e446554a5af2be30cbd1
user <user@DESKTOP-ISMAEL>
2024-02-24 08:53:08 -0600

    commit

--- 012024/src/main/java/commonMethods/GlobalVariables.java
+++ 012024/src/main/java/commonMethods/GlobalVariables.java
@@ -7,4 +7,6 @@ public class GlobalVariables {
 	public static final String PASSWORD = "secret_sauce";	
 	public static final String LOCKED_USER = "locked_out_user";	
 	
+	public static final String EXT_DATA = "./src/test/resources/";	
+	
 }
--- 012024/src/main/java/commonMethods/WrapClass.java
+++ 012024/src/main/java/commonMethods/WrapClass.java
@@ -1,8 +1,13 @@
 package commonMethods;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -39,6 +44,22 @@ public class WrapClass {
 			FileHandler.copy(scrFile, new File(screenshotPath + File.separatorChar + testCaseName + ".png"));
 		}catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public static String getJsonValue (String jsonFile, String jsonKey) {
+		//JsonData		
+		try {
+			InputStream inputStream = new FileInputStream (GlobalVariables.EXT_DATA + jsonFile + ".jason"); //USAMOS LA RUTA DONDE OBTENEMOS EL ARCHIVO JSON
+			JSONObject jsonObject = new JSONObject(new JSONTokener(inputStream));
+			
+			//LEER LOS DATOS
+			String jsonValue = jsonObject.getJSONObject(jsonFile).getString(jsonKey);
+			return jsonValue;
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
